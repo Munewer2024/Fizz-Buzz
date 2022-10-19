29796220bb2bea283a28a00933ed23d9d71a16bf
Munewer2024 <112985215+Munewer2024@users.noreply.github.com>
2022-10-19 14:24:26 -0700

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,25 @@
+public class FizzBuzz
+{
+    public void run() {
+        int a = 3;
+        int b = 5;
+        int c = 15;
+        for (int i = 1; i <= 100; i++) {
+            if (i == c) {
+                c += 15;
+                System.out.println("FizzBuzz");
+            } else if (i == a) {
+                a += 3;
+                System.out.println("Fizz");
+            } else if (i == b) {
+                b += 5;
+                System.out.println("Buzz");
+            } else System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz hw = new FizzBuzz();
+        hw.run();
+    }
+}
